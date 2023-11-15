48a24edaaaf1721ffbd0e91613bcea3d2138a899
Adrian Pilkington <50658445+AdrianPilko@users.noreply.github.com>
2023-11-15 19:08:40 +0000

    added comments to add.asm

--- z80RomEmulator/add.asm
+++ z80RomEmulator/add.asm
@@ -2,8 +2,8 @@
     ld a, $10
     ld b, $10    
     add a, b
-    ld hl, $0201
-    ld (hl), a
-    halt
-    ret   
+    ld hl, $0201 ;; load hl with place to store the result
+    ld (hl), a   ;; store the result of the addition into RAM
+    halt  
+    ret   ;; never gets here
 #END    
