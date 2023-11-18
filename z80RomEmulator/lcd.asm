912875c812c71a2fc3d806f513fb9eb49115d117
AdrianPilko <50658445+AdrianPilko@users.noreply.github.com>
2023-11-18 11:09:33 +0000

    more attempts

--- z80RomEmulator/lcd.asm
+++ z80RomEmulator/lcd.asm
@@ -11,50 +11,50 @@
 ;; The Z80 /RD (active low RD pin) NAND'd and connected to the R/W pin
 
 ;; datasheet for the lcd display: https://www.sparkfun.com/datasheets/LCD/HD44780.pdf
+;; datasheet for 74ls138 https://www.ti.com/lit/ds/symlink/sn74ls138.pdf
 ;; this all means:
-;;     to write to the display as a commands RS has to be set to 0
-;;     to write to the display as a data RS has to be set to 1
+;;     to write to the display as a commands RS has to be set to 0 ie z80 address pin 0 set to 0
+;;     to write to the display as a data RS has to be set to 1  ie z80 address pin 0 set to 1
 ;;     we don't need to worry about the R/W pin (enable as that's handled by NAND'd /RD)
-;;     we have to set the output port address to A5 high A6 low and A7 Low, the others apart from A0 fdon't matter
+;;     we have to set z80 A5 low A6 low and A7 Low, the others apart from A0 don't matter
 
 #define lcdRegisterSelectCommand $00
 #define lcdRegisterSelectData $01
-#define lcdEnableAddress $10    ; address pin 5 high on all other address set to zero, z80 only uses 255 ports with in/out instruction (8bit)
-#define enableLCDANDRSCommand $10
-#define enableLCDANDRSData $11
     
     .org 0
     
     ld a, $3f ; 8bit interface
-    out (enableLCDANDRSCommand), a
-    ld b, $ff   ; delay toop
+    out (lcdRegisterSelectCommand), a
+    ld b, $ff   ; delay loop
 delay1:
     ld a, $55   ; waste some clock cycles
     add a, b
     djnz delay1
 
     ld a,$0f    ; display on, cursor on
-    out (enableLCDANDRSCommand), a
+    out (lcdRegisterSelectCommand), a
 
-    ld b, $ff   ; delay toop
+    ld b, $ff   ; delay loop
 delay2:
     ld a, $55   ; waste some clock cycles
     add a, b
     djnz delay2
 
     ld a, $01    ; clear    
-    out (enableLCDANDRSCommand), a
+    out (lcdRegisterSelectCommand), a
+    ;;;; debug not even seeing display clear
+    ;halt
 
-    ld b, $ff   ; delay toop
+    ld b, $ff   ; delay loop
 delay3:
     ld a, $55   ; waste some clock cycles
     add a, b
     djnz delay3
 
     ld a, $06    ; left to right
-    out (enableLCDANDRSCommand), a
+    out (lcdRegisterSelectCommand), a
 
-    ld b, $ff   ; delay toop
+    ld b, $ff   ; delay loop
 delay4:
     ld a, $55   ; waste some clock cycles
     add a, b
@@ -66,10 +66,10 @@ writeTextLoop:
     ld a, (hl)
     cp $ff
     jp z, endProgram
-    out (enableLCDANDRSData), a
+    out (lcdRegisterSelectData), a
     inc hl
 
-    ld b, $ff   ; delay toop
+    ld b, $ff   ; delay loop
 delay5:
     ld a, $55   ; waste some clock cycles
     add a, b
@@ -78,8 +78,9 @@ delay5:
     jr writeTextLoop
 
 endProgram:
-    halt   
-
+    nop
+    jp endProgram
+    halt ; never gets here but 0x76 in assembly output easy to spot
 HELLO:    
     .db "Hello, world",$ff
 #END    
