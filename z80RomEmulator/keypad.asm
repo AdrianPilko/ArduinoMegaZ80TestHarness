0951b39002e4852ccb98e2797caccb3e6b46efd2
Adrian Pilkington <50658445+AdrianPilko@users.noreply.github.com>
2023-12-01 17:50:50 +0000

    Update keypad.asm

--- z80RomEmulator/keypad.asm
+++ z80RomEmulator/keypad.asm
@@ -1,33 +1,53 @@
 ;; port definitions
 #define lcdRegisterSelectCommand $00   ; all zero address including line 0 which is connected to the LCD  ReadSelect (RS) pin 
 #define lcdRegisterSelectData $01      ; all zero address except line 0 which is connected to the LCD  ReadSelect (RS) pin 
-#define keypadInOutPort $20             ; A6 high the rest low
+#define keypadInOutPort $20            ; A6 high the rest low
     
     .org 0   
     
     ld sp, $ffff
-    call initialiseLCD    
-keyboard_scan:          ;Loop for each row
-    call setLCDRow1
+    call initialiseLCD
+keyPadScan:
+    ld d, 0    
+    ld b, 8
     ld a, 00000001b
-    call getAndPrintKeypadA    
-    ld a, 00000010b
-    call getAndPrintKeypadA    
-    ld a, 00000100b
-    call getAndPrintKeypadA
-    ld a, 00001000b
-    call getAndPrintKeypadA
+keyboardRowScan: 
+    out (keypadInOutPort),a   ; put a out on databus on the keypad port        
+    in a, (keypadInOutPort)   ; read keypad port 
+
+    ;; a now contains the column of the key pressed 
+    ld e,a  
+    and a                     ; and'ing a with itself tells us if key was pressed
+    jp nz, keyFound
+    inc d
+    rla                       ; rotate a left one 
+    djnz keyPadScan
+    jp keyPadScan
+
+keyFound:
+    ;; row is in d, column is in e
+    ;; if we have 4x4
+    ;; 1 2 3 A
+    ;; 4 5 6 B
+    ;; 7 8 9 C
+    ;; # 0 . D 
+    ;; store in memory at keypadChars 123A456B789C#0.D     
+    ld b,e
+    ld hl, keypadChars
+keypadFindCharLoopCol:
+    inc hl   ; inc hl 4 times to offset to row a bit ineffcient
+    inc hl
+    inc hl
+    inc hl
+    djnz keypadFindCharLoop
+    ld b, d
+keypadFindCharLoopRow:
+    inc hl
+    djnz keypadFindCharLoopRow
+    ld a, (hl)
+    call hexprint8
     
-    call setLCDRow2
-    ld a, 00010000b
-    call getAndPrintKeypadA    
-    ld a, 00100000b
-    call getAndPrintKeypadA 
-    ld a, 01000000b
-    call getAndPrintKeypadA 
-    ld a, 10000000b
-    call getAndPrintKeypadA   
-    jr keyboard_scan    ;Loop infinitely (because this is a test)
+    jp keyPadScan
 
 getAndPrintKeypadA:
     out ($20),a           ;Output row
@@ -38,24 +58,24 @@ getAndPrintKeypadA:
     out (lcdRegisterSelectData), a    
     ret    
 
-hexprint8 		
-	push af ;store the original value of a for later
+hexprint8:		
+    push af ;store the original value of a for later
     call waitLCD 
     pop af
     push af ;store the original value of a for later
-	and $f0 ; isolate the first digit    
-	rrca
-	rrca
-	rrca
-	rrca  
+    and $f0 ; isolate the first digit    
+    rrca
+    rrca
+    rrca
+    rrca  
     call ConvertToASCII
-	out (lcdRegisterSelectData), a
+    out (lcdRegisterSelectData), a
     call waitLCD 
-	pop af ; retrieve original value of a
-	and $0f ; isolate the second digit
+    pop af ; retrieve original value of a
+    and $0f ; isolate the second digit
     call ConvertToASCII       
-	out (lcdRegisterSelectData), a
-	ret
+    out (lcdRegisterSelectData), a
+    ret
 
 ConvertToASCII:
     ; assuming the value in register a (0-15) to be converted to ascii
@@ -104,7 +124,7 @@ setLCDRow2:
 initialiseLCD:
     ld hl,InitCommandList
     call waitLCD
-loopLCDInitCommands
+loopLCDInitCommands:
     ld a, (hl)
     cp $ff
     jp z, initialiseLCDRET
@@ -116,4 +136,6 @@ initialiseLCDRET:
 ;;; rom "constants"
 InitCommandList:
     .db $38,$0e,$01,$06,$ff
+keypadChars:
+    .db 123A456B789C#0.D     
 #END
